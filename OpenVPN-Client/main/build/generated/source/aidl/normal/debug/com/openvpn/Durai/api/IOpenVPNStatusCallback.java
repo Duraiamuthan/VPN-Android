b2b919c2818d3cb10b218249718ba76bf65b1297
Duraiamuthan <h.duraiamuthan@gmail.com>
2015-07-24 03:44:31 +0530

    Android Temporary files
    Build

---
+++ OpenVPN-Client/main/build/generated/source/aidl/normal/debug/com/openvpn/Durai/api/IOpenVPNStatusCallback.java
@@ -0,0 +1,107 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ * Original file: /Users/duraiamuthan/Documents/Git/DIY-OpenVPN-Android/OpenVPN-Client/main/src/main/aidl/com/openvpn/Durai/api/IOpenVPNStatusCallback.aidl
+ */
+package com.openvpn.Durai.api;
+/**
+ * Example of a callback interface used by IRemoteService to send
+ * synchronous notifications back to its clients.  Note that this is a
+ * one-way interface so the server does not block waiting for the client.
+ */
+public interface IOpenVPNStatusCallback extends android.os.IInterface
+{
+/** Local-side IPC implementation stub class. */
+public static abstract class Stub extends android.os.Binder implements com.openvpn.Durai.api.IOpenVPNStatusCallback
+{
+private static final java.lang.String DESCRIPTOR = "com.openvpn.Durai.api.IOpenVPNStatusCallback";
+/** Construct the stub at attach it to the interface. */
+public Stub()
+{
+this.attachInterface(this, DESCRIPTOR);
+}
+/**
+ * Cast an IBinder object into an com.openvpn.Durai.api.IOpenVPNStatusCallback interface,
+ * generating a proxy if needed.
+ */
+public static com.openvpn.Durai.api.IOpenVPNStatusCallback asInterface(android.os.IBinder obj)
+{
+if ((obj==null)) {
+return null;
+}
+android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+if (((iin!=null)&&(iin instanceof com.openvpn.Durai.api.IOpenVPNStatusCallback))) {
+return ((com.openvpn.Durai.api.IOpenVPNStatusCallback)iin);
+}
+return new com.openvpn.Durai.api.IOpenVPNStatusCallback.Stub.Proxy(obj);
+}
+@Override public android.os.IBinder asBinder()
+{
+return this;
+}
+@Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+{
+switch (code)
+{
+case INTERFACE_TRANSACTION:
+{
+reply.writeString(DESCRIPTOR);
+return true;
+}
+case TRANSACTION_newStatus:
+{
+data.enforceInterface(DESCRIPTOR);
+java.lang.String _arg0;
+_arg0 = data.readString();
+java.lang.String _arg1;
+_arg1 = data.readString();
+java.lang.String _arg2;
+_arg2 = data.readString();
+java.lang.String _arg3;
+_arg3 = data.readString();
+this.newStatus(_arg0, _arg1, _arg2, _arg3);
+return true;
+}
+}
+return super.onTransact(code, data, reply, flags);
+}
+private static class Proxy implements com.openvpn.Durai.api.IOpenVPNStatusCallback
+{
+private android.os.IBinder mRemote;
+Proxy(android.os.IBinder remote)
+{
+mRemote = remote;
+}
+@Override public android.os.IBinder asBinder()
+{
+return mRemote;
+}
+public java.lang.String getInterfaceDescriptor()
+{
+return DESCRIPTOR;
+}
+/**
+     * Called when the service has a new status for you.
+     */
+@Override public void newStatus(java.lang.String uuid, java.lang.String state, java.lang.String message, java.lang.String level) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeString(uuid);
+_data.writeString(state);
+_data.writeString(message);
+_data.writeString(level);
+mRemote.transact(Stub.TRANSACTION_newStatus, _data, null, android.os.IBinder.FLAG_ONEWAY);
+}
+finally {
+_data.recycle();
+}
+}
+}
+static final int TRANSACTION_newStatus = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+}
+/**
+     * Called when the service has a new status for you.
+     */
+public void newStatus(java.lang.String uuid, java.lang.String state, java.lang.String message, java.lang.String level) throws android.os.RemoteException;
+}
